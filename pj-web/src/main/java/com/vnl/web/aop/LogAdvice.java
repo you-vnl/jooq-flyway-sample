2d1f5ae1b9b1372c4676124b9d8927137be87a06
you-vnl <k-youth-75@hotmail.co.jp>
2020-04-23 21:00:01 +0900

    ログAOPクラスの実装

---
+++ pj-web/src/main/java/com/vnl/web/aop/LogAdvice.java
@@ -0,0 +1,32 @@
+package com.vnl.web.aop;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.stereotype.Component;
+
+/**
+ * ログAOPクラスです。
+ */
+@Component
+@Aspect
+@Slf4j
+public class LogAdvice {
+
+    @Before("execution(* com.vnl.web.usecase.*.*(..))")
+    public static void invokeBeforeUseCase(final JoinPoint joinPoint) {
+        methodLog(joinPoint.getTarget().getClass().toString(), joinPoint.getSignature().getName(), "start");
+    }
+
+    @After("execution(* com.vnl.web.usecase.*.*(..))")
+    public static void invokeAfterUseCase(final JoinPoint joinPoint) {
+        methodLog(joinPoint.getTarget().getClass().toString(), joinPoint.getSignature().getName(), "end");
+    }
+
+    private static void methodLog(final String className, final String methodName, final String message) {
+        log.info("{}.{}() {}.", className, methodName, message);
+    }
+
+}
